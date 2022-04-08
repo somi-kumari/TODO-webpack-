0bc24695c6e4a518e289bbb1b124fff9c619d0f6
somi-kumari <somisourabh96@gmail.com>
2022-04-08 10:29:52 +0530

    final todo list (react webpack)

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./index.css";
+import "./todo.js";
+const root = document.getElementById("root1");
+const img = document.createElement("img");
+img.src = "https://i.pinimg.com/originals/5e/81/9a/5e819a5ce865476b73087fd1276e7c3e.png";
+img.setAttribute = ("id", "image");
+
+root.appendChild(img)
+
+
